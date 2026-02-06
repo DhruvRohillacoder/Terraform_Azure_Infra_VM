77575626af1f0eec048b9a6fa5924dc5e9bb534f
Dhruv <dhruvrohilla9350@gmail.com>
2026-02-06 23:10:21 +0530

    add bd

---
+++ ENV/DEV/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+backend "azurerm" {
+  resource_group_name  = "rg-titumama-1"
+  storage_account_name = "sadevopsinsiders123"
+  container_name       = "terraform-state"
+  key                  = "dhondhu.terraform.tfstate"
+}
+}
