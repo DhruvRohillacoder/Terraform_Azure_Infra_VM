ba8738ab053b0e59ec4ae0e1332fd68959a03ccf
Dhruv <dhruvrohilla9350@gmail.com>
2026-02-07 00:54:39 +0530

    bd set

--- ENV/DEV/provider.tf
+++ ENV/DEV/provider.tf
@@ -2,13 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.58.0"
+      version = "4.59.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
   features {}
   subscription_id = "bf9b19d0-a2bd-49ee-9a16-6b8232861da6"
-} 
+}
