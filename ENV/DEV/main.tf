17d262979c7975420b36cf31c431345fc72abbb9
Dhruv <dhruvrohilla9350@gmail.com>
2026-02-06 21:39:54 +0530

    add type and description and default name in Rg

--- ENV/DEV/main.tf
+++ ENV/DEV/main.tf
@@ -1,5 +1,3 @@
 module "rg" {
   source = "../../Modules/azurerm_resource_group"
-  name     = "rg-dev-001"
-  location = "eastus"
 }
--- Modules/azurerm_resource_group/variable.tf
+++ Modules/azurerm_resource_group/variable.tf
@@ -1,2 +1,10 @@
- variable name{}
- variable location {}
+ variable name{
+    type = string
+    description = "The name of the Resource Group"
+    default = "rg001"
+ }
+ variable location {
+    type = string
+    description = "The location of the Resource Group"
+    default = "eastus"
+ }
