853f41f75b8e8c224b58b7f04477e598aced483a
Dhruv <dhruvrohilla9350@gmail.com>
2026-02-09 21:33:08 +0530

    add rg with count

--- ENV/DEV/main.tf
+++ ENV/DEV/main.tf
@@ -1,18 +1,3 @@
 module "rg" {
   source = "../../Modules/azurerm_resource_group"
 }
-
-module "vnet" {
-  source     = "../../Modules/azurerm_virtual_network"
-  depends_on = [module.rg]
-}
-
-module "subnet" {
-  source     = "../../Modules/azurerm_subnet"
-  depends_on = [module.vnet]
-}
-
-module "pip" {
-  source = "../../Modules/azurerm_public_ip"
-  depends_on = [ module.rg ]
-}
--- Modules/azurerm_resource_group/main.tf
+++ Modules/azurerm_resource_group/main.tf
@@ -1,5 +1,6 @@
   #count -> creat 10 rg with different name and location with count
 resource "azurerm_resource_group" "rg" {
-  name     = ""
-  location = ""
+  count = 5
+  name     = "rg-count-${count.index}"
+  location = "eastus"
 }
